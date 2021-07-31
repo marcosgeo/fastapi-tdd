28d56dcd653f092cbebaabc6c446db54884638f3
Antonio Marcos <marcosgeo@yahoo.com>
2021-07-31 12:18:56 -0300

    deployment: made prod version and deployed to heroku. code coverage and quality: added cov, flake8, black and sort for quality control.

--- project/app/main.py
+++ project/app/main.py
@@ -1,5 +1,6 @@
 import logging
-from fastapi import FastAPI, Depends
+
+from fastapi import FastAPI
 
 from app.api import ping, summaries
 from app.db import init_db
@@ -10,9 +11,12 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
-    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
 
     return application
+
 
 app = create_application()
 
